ff0c5894b73b23775238f9a66b2ff53d59dd5157
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-06-01 18:21:32 +0700

    Remove boost dependency since it's unused

--- test/conanfile.py
+++ test/conanfile.py
@@ -4,7 +4,7 @@ from conan import ConanFile
 class Recipe(ConanFile):
     settings = ["os", "compiler", "build_type", "arch"]
     generators = ["CMakeToolchain", "CMakeDeps"]
-    requires = ["boost/1.83.0", "boost-ext-ut/1.1.9", "fmt/10.2.1"]
+    requires = ["boost-ext-ut/1.1.9", "fmt/10.2.1"]
 
     def layout(self):
         self.folders.generators = "conan"
