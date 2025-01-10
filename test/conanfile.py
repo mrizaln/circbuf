a3592842d8ab556f76ede7cbdb14f560791f7e38
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-01-10 15:49:09 +0700

    Revert ut version

--- test/conanfile.py
+++ test/conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.cmake import cmake_layout
 class Recipe(ConanFile):
     settings = ["os", "compiler", "build_type", "arch"]
     generators = ["CMakeToolchain", "CMakeDeps"]
-    requires = ["boost-ext-ut/2.0.1", "fmt/10.2.1"]
+    requires = ["boost-ext-ut/1.1.9", "fmt/10.2.1"]
 
     def layout(self):
         cmake_layout(self)
